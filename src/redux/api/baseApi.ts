c0b59e5344949c6276aed2ed9111288a2d1273b1
noyon-podder <noyon.podder7@gmail.com>
2024-07-14 19:40:32 +0600

    base api connect

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://mk-server-psi.vercel.app/api/v1",
+  }),
   endpoints: (builder) => ({
     getProduct: builder.query({
       query: () => ({
